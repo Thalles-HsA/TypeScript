232e03aff6c92de6ea1c65ed8be325bff47bf882
Thalles-HsA <thsa.henrique@gmail.com>
2023-01-30 18:13:29 -0300

    Primeiros passos com TypeScript

---
+++ app/models/negociacao.ts
@@ -0,0 +1,17 @@
+export class Negociacao {
+
+    constructor(
+        private _data: Date, 
+        readonly quantidade: number, 
+        readonly valor: number
+    ) {}
+
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+}
