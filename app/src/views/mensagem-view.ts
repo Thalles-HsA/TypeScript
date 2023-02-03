b9ce558ec5560ac158764032efa3a1d58d6fc0d6
Thalles-HsA <thsa.henrique@gmail.com>
2023-02-02 22:41:53 -0300

    evoluindo em decorator

--- app/src/views/mensagem-view.ts
+++ app/src/views/mensagem-view.ts
@@ -1,7 +1,9 @@
+import { escape } from "../decorators/escapar.js";
 import { View } from "./view.js";
 
 export class MensagemView  extends View<string>{
 
+    @escape
     protected template(model: string): string {
 
         return `
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,9 +1,12 @@
+import { inspect } from "../decorators/inspect.js";
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
+ 
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor)
 
         if(elemento){
@@ -11,17 +14,12 @@ export abstract class View<T> {
         } else {
             throw Error (`Seletor ${seletor} não existe no DOM`)
         }
-
-        if(escapar) {
-            this.escapar = escapar;
-        }
     }
+
 
     public update(model: T): void {
         let template = this.template(model);
-        if(this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
+    
         this.elemento.innerHTML = template;
     }
 
